058b9c0092b34e71f10190f88d96f96b9b535347
Meastro-code <codemaestro87@gmail.com>
2023-06-15 14:53:08 -0700

    sizeof()

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int i;
 long int m;
 long long int p;
 float h;
-printf("Size of a char:%lu\n",(unsignedint)sizeof(c));
-printf("Size of an int:%lu\n",(unsignedint)sizeof(i));
-printf("Size of a long int:%lu\n",(unsignedint)sizeof(m));
-printf("Size of a long long int:%lu\n",(unsignedint)sizeof(p));
-printf("Size of a float:%lu\n",(unsignedint)sizeof(h));
+printf("Size of a char:%d\n",(int)sizeof(c));
+printf("Size of an int:%d\n",(int)sizeof(i));
+printf("Size of a long int:%d\n",(int)sizeof(m));
+printf("Size of a long long int:%d\n",(int)sizeof(p));
+printf("Size of a float:%d\n",(int)sizeof(h));
 return (0);
 }
