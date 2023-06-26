1b6d62715e3a9846cd86c3109dc119d58f5281f8
Meastro-code <codemaestro87@gmail.com>
2023-06-26 12:07:00 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to the first character
+ *
+ * This function takes a pointer to
+ * a character as its parameter and returns
+ * the length of the string pointed to by s.
+ * The function iterates through the
+ * string, counting the number of characters
+ * until it reaches the null character.
+ */
+int _strlen(char *s)
+{
+int length = 0;
+while (*s != '\0')
+{
+length++;
+s++;
+}
+return length;
+}
