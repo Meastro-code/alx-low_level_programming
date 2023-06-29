654513ee7e02e7288475f5a5fdd0f2f4d4e77f9b
Meastro-code <codemaestro87@gmail.com>
2023-06-29 08:46:37 -0700

    concatinating strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+/**
+ * char *_strcat - concatenates two strings
+ * @*dest: the first string
+ * @*src: the second string
+ *
+ * Return: *dest
+ */
+char *_strcat(char *dest, char *src)
+{
+int i, j;
+for (i = 0; dest[i] != '\0'; i++)
+{
+}
+for (j = 0; src[j] != '\0'; j++)
+{
+dest[i + j] = src[j];
+}
+dest[i + j] = '\0';
+return (dest);
+}
