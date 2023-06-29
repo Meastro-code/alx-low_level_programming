6ddd936f98dd1c3f8f0d405b974a1d0ca4912bb5
Meastro-code <codemaestro87@gmail.com>
2023-06-29 10:26:26 -0700

    capitalise first lettter

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+/**
+ * *cap_string - capitalise letters
+ * @s: string
+ * 
+ * Return: string
+ */
+char *cap_string(char *s)
+{
+int i;
+char prev_char;
+
+for (i = 0, prev_char = s[0]; s[i] != '\0'; i++)
+{
+if ((prev_char == ' ' || prev_char == '\t' || prev_char == '\n' ||
+prev_char == ',' || prev_char == ';' || prev_char == '.' ||
+prev_char == '!' || prev_char == '?' || prev_char == '"' ||
+prev_char == '(' || prev_char == ')' || prev_char == '{' ||
+prev_char == '}') && (s[i] >= 'a' && s[i] <= 'z'))
+{
+s[i] = s[i] - 'a' + 'A';
+}
+prev_char = s[i];
+}
+return (s);
+}
