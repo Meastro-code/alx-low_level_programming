7967bc137059ce82d94b7e64d0d82a5f1c9f207e
Meastro-code <codemaestro87@gmail.com>
2023-07-03 10:37:24 -0700

    search for substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include <stddef.h>
+
+/**
+ * _strstr - locates substring
+ * @haystack: first string
+ * @needle: second string
+ *
+ * Return: string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+unsigned int i, j;
+for (i = 0; haystack[i] != '\0'; i++)
+{
+for (j = 0; needle[j] != '\0'; j++)
+{
+if (haystack[i + j] != needle[j])
+{
+break;
+}
+}
+if (needle[j] == '\0')
+{
+return (&haystack[i]);
+}
+}
+return (NULL);
+}
