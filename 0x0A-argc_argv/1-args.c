2f975586a388eac6fcf195aebab3967e873093fe
Meastro-code <codemaestro87@gmail.com>
2023-07-10 11:19:55 -0700

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - counts arguments
+ * @argc: first parameter
+ * @argv: second parameter
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+if (*argv[0])
+{
+return (0);
+}
+return (0);
+}
