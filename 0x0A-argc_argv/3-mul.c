60defcfcfd4b1b6f369912f0123e64e3ebff953e
Meastro-code <codemaestro87@gmail.com>
2023-07-10 11:35:35 -0700

    multiply command line args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies
+ * @argc: first parameter
+ * @argv: second parameter
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+int num1, num2;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+printf("%d\n", num1 * num2);
+return (0);
+}
