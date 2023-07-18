43c3f0c0c2f7c6cc5fe378c5797da45b9ae0f0df
Meastro-code <codemaestro87@gmail.com>
2023-07-18 13:26:04 -0700

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+
+/**
+ * struct dog - data type
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: data stored in type
+ */
+struct dog {
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif /* _SAMPLE_HEADER_H_ */
