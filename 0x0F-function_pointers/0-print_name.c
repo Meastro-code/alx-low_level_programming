d803f63789c9f4fc1431bd7bfe59f3dc767c1b5d
Meastro-code <codemaestro87@gmail.com>
2023-07-19 10:45:52 -0700

    function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,9 +3,7 @@
 /**
  * print_name - Prints a name using a specified function.
  * @name: Pointer to the name to be printed.
- * @f: Pointer to the function that formats and prints the name.
- *
- * Description: Calls 'f', passing 'name' as an argument. 'f' handles printing.
+ * @f: Pointer to function that formats and prints name.
  */
 void print_name(char *name, void (*f)(char *))
 {
